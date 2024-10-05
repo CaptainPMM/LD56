7b25776757ba2fbf8b7f31bd44b394a630a7e83f
Lennart R. <lr080@hdm-stuttgart.de>
2024-10-05 19:24:27 +0200

    Added Color cube

---
+++ scripts/holz_wuerfel.gd
@@ -0,0 +1,31 @@
+extends Node3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	var randomVal = rng.randi_range(0, 25)
+	var num = 65 + randomVal;
+	
+	
+	$S1.text = String.chr(num);
+	$S2.text = String.chr(num);
+	$S3.text = String.chr(num);
+	$S4.text = String.chr(num);
+	$S5.text = String.chr(num);
+	$S6.text = String.chr(num);
+	
+	$Cube.mesh.surface_get_material(1).albedo_color = Color(rng.randi_range(1, 4) / 4.0, rng.randi_range(1, 4) / 4.0, rng.randi_range(1, 4) / 4.0)
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func get_all_children(in_node,arr:=[]):
+	arr.push_back(in_node)
+	for child in in_node.get_children():
+		arr = get_all_children(child,arr)
+	return arr
