1a2ba17e5606c8c950c9da861af091a5f6fb813d
Timo Eberl <timo@timoeberl.de>
2024-10-06 14:06:00 +0200

    Use only 1 MeshInstance3D per dude -> Avoid running into web limit of 1000x MeshInstance3D

--- scripts/furball.gd
+++ scripts/furball.gd
@@ -3,7 +3,7 @@ class_name Furball
 
 
 @export_group("Setup")
-@export var shell_mesh: Mesh
+@export var shell_mesh: ArrayMesh
 @export var shell_material: ShaderMaterial
 @export var shell_scene: PackedScene
 
@@ -25,19 +25,29 @@ class_name Furball
 @export_range(0, 1) var displacement_strength: float = 0.1
 
 var displacement_vector: Vector3 = Vector3.ZERO
+var shells: Shell
 
 func _ready() -> void:
 	$Preview.visible = false
+	
+	# generate mesh that has one surface for every shell
+	var mdt: MeshDataTool = MeshDataTool.new()
+	mdt.create_from_surface(shell_mesh, 0)
+	var multi_surface_mesh: ArrayMesh = ArrayMesh.new()
 	for i in shell_count:
-		var shell: Shell = shell_scene.instantiate()
-		$Shells.add_child(shell)
-		shell.name = str("Shell ", i)
-		shell.mesh_instance.mesh = shell_mesh
-		if i != 0: # shadow only on one mesh
-			shell.mesh_instance.cast_shadow = GeometryInstance3D.SHADOW_CASTING_SETTING_OFF
+		mdt.commit_to_surface(multi_surface_mesh)
+
+	shells = shell_scene.instantiate()
+	self.add_child(shells)
+	shells.name = str("Shells")
+	shells.mesh_instance.mesh = multi_surface_mesh
+	# disable shadow
+	shells.mesh_instance.cast_shadow = GeometryInstance3D.SHADOW_CASTING_SETTING_OFF
+	
+	for i in shell_count:
 		var mat_duplicate: ShaderMaterial = shell_material.duplicate()
 		update_properties_material(mat_duplicate, i)
-		shell.mesh_instance.material_override = mat_duplicate
+		shells.mesh_instance.set_surface_override_material(i, mat_duplicate)
 
 func _process(_delta: float) -> void:
 	# update all shader globals every frame in editor or debug build, but not in release build
@@ -54,16 +64,14 @@ func _physics_process(delta: float) -> void:
 	displacement_vector -= pos_change * 3 / glob_scale
 	displacement_vector.y -= delta * 10
 	
-	var len: float = 3
-	if (displacement_vector.length() > len):
-		displacement_vector = displacement_vector.normalized() * len
+	var l: float = 3
+	if (displacement_vector.length() > l):
+		displacement_vector = displacement_vector.normalized() * l
 	update_physics_all_materials()
 
 func update_physics_all_materials() -> void:
-	var children: Array[Node] = $Shells.get_children()
-	for i in children.size():
-		var shell: Shell = children[i]
-		var mat: ShaderMaterial = shell.mesh_instance.material_override
+	for i in shells.mesh_instance.get_surface_override_material_count():
+		var mat: ShaderMaterial = shells.mesh_instance.get_surface_override_material(i)
 		var glob_scale = self.global_transform.basis.get_scale()
 		# quick hack: make vertical movement more visible
 		mat.set_shader_parameter(
@@ -72,10 +80,9 @@ func update_physics_all_materials() -> void:
 		)
 
 func update_properties_all_materials() -> void:
-	var children: Array[Node] = $Shells.get_children()
-	for i in children.size():
-		var shell: Shell = children[i]
-		update_properties_material(shell.mesh_instance.material_override, i)
+	for i in shells.mesh_instance.get_surface_override_material_count():
+		var mat: ShaderMaterial = shells.mesh_instance.get_surface_override_material(i)
+		update_properties_material(mat, i)
 
 func update_properties_material(mat: ShaderMaterial, shell_index: int) -> void:
 	mat.set_shader_parameter("u_shell_index", shell_index)
